68726d74a3425186890424d640600349e107b8c9
mazbaul20 <mazbaul20@gmail.com>
2025-02-13 17:48:53 +0600

    added backend part 1 and 2

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $superAdmin = User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        //create role
+        $role = Role::create(['name'=>'Admin']);
+
+        //Assign Permissions to Role
+        $permission = Permission::pluck('id')->all();
+        $role->syncPermissions($permission);
+
+        //Assign Role to User
+        $superAdmin->assignRole($role);
+        $admin->syncRoles($role);
+    }
+}
